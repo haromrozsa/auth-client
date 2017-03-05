b8b5f21adb16397c2d6e45c543c6e3208e69cbd7
haromrozsa <haromrozsa@gmail.com>
2017-03-05 14:37:00 +0100

    Finish client auth

---
+++ src/components/auth/signin.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { reduxForm} from 'redux-form';
+import * as actions from '../../actions'
+
+class Signin extends Component {
+  handleFormSubmit({ email, password }) {
+    console.log(email, password);
+    this.props.signinUser({email, password});
+
+  }
+
+  renderAlert() {
+    if (this.props.errorMessage) {
+        return (
+          <div className="alert alert-danger">
+            <strong>Oops!</strong> {this.props.errorMessage}
+          </div>
+        );
+    }
+
+  }
+
+  render() {
+    const { fields: { email, password }, handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input type="text" className="form-control" {...email}/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input {...password} type="password" className="form-control" />
+        </fieldset>
+        {this.renderAlert()}
+        <button action="submit" className="btn btn-primary">Sign in</button>
+      </form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
+export default reduxForm({
+  form: 'signin',
+  fields: ['email', 'password']
+}, mapStateToProps, actions)(Signin);
